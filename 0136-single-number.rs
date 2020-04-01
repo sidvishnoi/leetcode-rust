a5a5972113c6b46a056ffaaf1e42fc32eec3fd5b
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-04-01 16:54:28 +0530

    add: Single Number

---
+++ 0136-single-number.rs
@@ -0,0 +1,12 @@
+struct Solution;
+
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        nums.iter().fold(0, |acc, x| acc ^ x)
+    }
+}
+
+fn main() {
+    assert_eq!(1, Solution::single_number(vec![2, 2, 1]));
+    assert_eq!(4, Solution::single_number(vec![4, 1, 2, 1, 2]));
+}
