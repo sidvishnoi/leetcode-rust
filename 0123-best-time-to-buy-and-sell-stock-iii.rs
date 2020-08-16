4c5b798ba57149d6c0bbbb8c810655503e08f19a
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-08-16 15:50:31 +0530

    perf(Best Time to Buy and Sell Stock III): simplify for two transactions

--- 0123-best-time-to-buy-and-sell-stock-iii.rs
+++ 0123-best-time-to-buy-and-sell-stock-iii.rs
@@ -1,39 +1,21 @@
 struct Solution;
 
-use std::cmp;
-// With k = max_transactions and n = number of days (length of prices)
-// Time: O(kn)
-// Space: O(k)
+use std::cmp::max;
+// Time: O(n)
+// Space: O(1)
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        if prices.is_empty() {
-            return 0;
+        let mut one_buy = std::i32::MIN;
+        let mut one_buy_one_sell = 0;
+        let mut two_buy = std::i32::MIN;
+        let mut two_buy_two_sell = 0;
+        for price in prices {
+            one_buy = max(one_buy, -price);
+            one_buy_one_sell = max(one_buy_one_sell, price + one_buy);
+            two_buy = max(two_buy, one_buy_one_sell - price);
+            two_buy_two_sell = max(two_buy_two_sell, two_buy + price);
         }
-
-        let max_transactions: usize = 2;
-
-        // profits[k][i] = profit with k transactions until i'th day.
-        // Optimization: As the profit for a day only depends on previous day,
-        // we don't need to store profit for all days.
-        let mut profits = vec![0; max_transactions + 1];
-
-        // profits[k][i] = max(
-        //     // if we don't trade, profit is same as previous day:
-        //     profits[k, i - 1],
-        //     // if we bought previous share on j'th day and sold on i'th day:
-        //     prices[i] - prices[j] + profits[k-1][j-1] for j=[0..i-1]
-        //     // ...which is same as: subtracting min of (prices[j] + profits[k-1][j-1]) from prices[i]
-        //     // also, we can avoid repeating calculation of min above.
-        // )
-        let mut min = vec![prices[0]; max_transactions + 1];
-        for i in 1..prices.len() {
-            for k in 1..=max_transactions {
-                min[k] = cmp::min(min[k], prices[i] - profits[k - 1]);
-                profits[k] = cmp::max(profits[k], prices[i] - min[k]);
-            }
-        }
-
-        profits[max_transactions]
+        two_buy_two_sell
     }
 }
 
