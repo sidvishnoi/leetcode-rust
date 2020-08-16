3fc51c6d960c93dc15c88dda843d03dacc1222ae
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-08-16 15:31:37 +0530

    perf(Best Time to Buy and Sell Stock III): avoid repeated calculation

--- 0123-best-time-to-buy-and-sell-stock-iii.rs
+++ 0123-best-time-to-buy-and-sell-stock-iii.rs
@@ -2,7 +2,7 @@ struct Solution;
 
 use std::cmp;
 // With k = max_transactions and n = number of days (length of prices)
-// Time: O(kn^2)
+// Time: O(kn)
 // Space: O(kn)
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
@@ -21,13 +21,12 @@ impl Solution {
         //     // if we bought previous share on j'th day and sold on i'th day:
         //     prices[i] - prices[j] + profits[k-1][j-1] for j=[0..i-1]
         //     // ...which is same as: subtracting min of (prices[j] + profits[k-1][j-1]) from prices[i]
+        //     // also, we can avoid repeating calculation of min above.
         // )
         for k in 1..=max_transactions {
+            let mut min = prices[0];
             for i in 1..prices.len() {
-                let mut min = prices[0];
-                for j in 1..=i {
-                    min = cmp::min(min, prices[j] - profits[k - 1][j - 1]);
-                }
+                min = cmp::min(min, prices[i] - profits[k - 1][i - 1]);
                 profits[k][i] = cmp::max(profits[k][i - 1], prices[i] - min);
             }
         }
